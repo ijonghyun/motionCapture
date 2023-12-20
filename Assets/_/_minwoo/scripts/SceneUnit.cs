f77f72b69ce6c7ce2f3fad751cfc6dc31efbe0f8
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-12-20 17:05:51 +0900

    anim, cinemachin edit

---
+++ Assets/_/_minwoo/scripts/SceneUnit.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneUnit : MonoBehaviour
+{
+    [SerializeField] List<GameObject> actors = new List<GameObject>();
+}
